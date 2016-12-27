82ec3c70e4f92707340f293306c64cc99255353b
Raman A. Shah <raman.anand.shah@gmail.com>
2016-12-27 17:31:21 -0600

    *.py: Fix shebangs. Vim muscle memory on emacs...

--- nonaxisymmetric/nonaxisymmetric.py
+++ nonaxisymmetric/nonaxisymmetric.py
@@ -1,4 +1,4 @@
-#!i /usr/bin/env python
+#! /usr/bin/env python
 #
 # oravg.py - An engine to calculate orientation averaged cross sections
 # and orientation-dependent spectral information starting from several
